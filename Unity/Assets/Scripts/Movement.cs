0bcc6f4f925fc7d7ffe20abb1706f33e4b25e4f7
Psytew <patrickwestervelt.us@gmail.com>
2019-10-26 23:27:34 -0400

    First commit

---
+++ Unity/Assets/Scripts/Movement.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Movement : MonoBehaviour
+{
+
+    public float movementSpeed = 0.3f;
+    public float sideSpeed = 0.1f;
+    public float rotSpeed = 40.0f;
+    public Animator anim;
+
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W) || (Input.GetKeyDown(KeyCode.S)) || (Input.GetKeyDown(KeyCode.A)) || (Input.GetKeyDown(KeyCode.D)))
+        {
+            anim.Play("BeginWalk");
+        }
+        if (Input.GetKeyDown(KeyCode.E) || (Input.GetKeyDown(KeyCode.Q)))
+        {
+            anim.Play("BeginDiag");
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            GetComponent<Rigidbody>().position -= transform.forward * Time.deltaTime * movementSpeed;
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            GetComponent<Rigidbody>().position -= transform.right * Time.deltaTime * sideSpeed;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            GetComponent<Rigidbody>().position += transform.right * Time.deltaTime * sideSpeed;
+        }
+        else if (Input.GetKey(KeyCode.E))
+        {
+            transform.Rotate(0, Time.deltaTime * rotSpeed, 0);
+        }
+        else if (Input.GetKey(KeyCode.Q))
+        {
+            transform.Rotate(0, Time.deltaTime * -rotSpeed, 0);
+        }
+        else if (Input.GetKeyUp(KeyCode.W) || (Input.GetKeyUp(KeyCode.S)) || (Input.GetKeyUp(KeyCode.A)) || (Input.GetKeyUp(KeyCode.D)))
+        {
+            anim.Play("EndWalk");
+        }
+        else if (Input.GetKeyUp(KeyCode.E) || (Input.GetKeyUp(KeyCode.Q)))
+        {
+            anim.Play("EndDiag");
+        }
+    }
+}
